2dd060997d78d45f85a1f4ca717d2870fe4673f2
Eletromaximus <maxmillianox@protonmail.com>
2020-04-22 15:54:41 -0300

    Adição do useInternal hook para a contagem do contador

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,25 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 
 
 function App() {
+  const [time, setTime] = useState(0);
+  const [isActive, setIsActive] = useState(true);
+  const [mode, setMode] = useState('session');
+
+  
+  function toggle () {
+    setIsActive(!isActive);
+  }
+
+  useEffect(() => {
+    if(isActive){
+      setTime(+1);
+    }
+  },[isActive, time])
+
   return (
     <div className="App">
-      
+      The time is {time}
     </div>
   );
 }
---
+++ src/Hooks/interval.ts
@@ -0,0 +1,29 @@
+import {useEffect, useRef} from 'react';
+
+
+const useInterval = (callback:any, delay:number) => {
+  const savedCallback = useRef<any|undefined>();
+
+
+  useEffect(
+      () => {
+          savedCallback.current = callback;
+      },
+      [callback]
+  );
+
+  useEffect(
+      () => {
+          const handler = (...args:any) => savedCallback.current(...args );
+
+          if (delay != null) {
+              const id = setInterval(handler, delay);
+              return() => clearInterval(id);
+          }
+
+      },
+      [delay]
+  );
+}
+
+export default useInterval;
