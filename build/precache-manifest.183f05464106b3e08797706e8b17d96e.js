8a707eb593506382c4689cf61d98c778ef6f5191
Eletromaximus <maxmillianox@protonmail.com>
2020-05-07 10:47:17 -0300

    Adicionando build

---
+++ build/precache-manifest.183f05464106b3e08797706e8b17d96e.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "720508183e0523082c547767d6e12161",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7646351c64838ee02409",
+    "url": "/static/css/2.f524894f.chunk.css"
+  },
+  {
+    "revision": "ee07704f24579a99a97c",
+    "url": "/static/css/main.32665ff7.chunk.css"
+  },
+  {
+    "revision": "7646351c64838ee02409",
+    "url": "/static/js/2.230f0f8a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.230f0f8a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ee07704f24579a99a97c",
+    "url": "/static/js/main.f7da161d.chunk.js"
+  },
+  {
+    "revision": "a2fae4788ac46535fc59",
+    "url": "/static/js/runtime-main.7b8214b8.js"
+  },
+  {
+    "revision": "1467e2d37ec60cc50117a8db7c24f626",
+    "url": "/static/media/DS-DIGIB.1467e2d3.TTF"
+  }
+]);
