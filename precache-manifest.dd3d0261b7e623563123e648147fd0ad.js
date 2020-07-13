ee7a815a0cf4a00d949e3361387e8d0b50a63ce7
Eletromaximus <maxmillianox@protonmail.com>
2020-07-13 20:25:07 -0300

    Updates

---
+++ precache-manifest.dd3d0261b7e623563123e648147fd0ad.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0f9b3ca1b0a33c520965697f78d544bb",
+    "url": "/PomodoroClock/index.html"
+  },
+  {
+    "revision": "f46bf3aad4ef504554bf",
+    "url": "/PomodoroClock/static/css/2.f524894f.chunk.css"
+  },
+  {
+    "revision": "42327c58ab14467b4fa4",
+    "url": "/PomodoroClock/static/css/main.042ef690.chunk.css"
+  },
+  {
+    "revision": "f46bf3aad4ef504554bf",
+    "url": "/PomodoroClock/static/js/2.104989f7.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/PomodoroClock/static/js/2.104989f7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cc14dac1efdae9071022",
+    "url": "/PomodoroClock/static/js/3.128f103c.chunk.js"
+  },
+  {
+    "revision": "287e48aeac3564a8b2b5685a67fb9216",
+    "url": "/PomodoroClock/static/js/3.128f103c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "42327c58ab14467b4fa4",
+    "url": "/PomodoroClock/static/js/main.e39a6a47.chunk.js"
+  },
+  {
+    "revision": "43606be6fbf9062b4ac9",
+    "url": "/PomodoroClock/static/js/runtime-main.0f044efe.js"
+  },
+  {
+    "revision": "19700b739837b47ca934707f1c16e7b7",
+    "url": "/PomodoroClock/static/media/377639__danarobinsondesignsgmailcom__b15.19700b73.mp3"
+  },
+  {
+    "revision": "1467e2d37ec60cc50117a8db7c24f626",
+    "url": "/PomodoroClock/static/media/DS-DIGIB.1467e2d3.TTF"
+  }
+]);
