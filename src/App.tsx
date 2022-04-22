0ed37c80c31ea629d52273c5e78c7cc45a9b9f9b
Max Milliano <maxmillianox@protonmail.com>
2022-04-22 16:11:12 -0300

    feat(atualizando pagina de configuração de sessões, minutos e descanço): mudanças no estilo e setas
    adicionando removendo e adicionando propriedades. criar futuramente paginas de descanço e trabalho.

--- src/App.tsx
+++ src/App.tsx
@@ -1,49 +1,163 @@
 import React, { useState, useEffect } from 'react'
+import ArrowCircleDownIcon from '@mui/icons-material/ArrowCircleDown'
+import ArrowCircleUpIcon from '@mui/icons-material/ArrowCircleUp'
 // import useSound from 'use-sound'
 
-// import 'react-circular-progressbar/dist/styles.css'
-// import './App.scss'
-// import './CircularProgress.scss'
+import './App.scss'
 // @ts-ignore
 // import sound from './Timer/377639__danarobinsondesignsgmailcom__b15.mp3'
 
 export default function App () {
-  const [minutes, setMinutes] = useState(25)
-  const [seconds, setSeconds] = useState(60)
+  const [minutes, setMinutes] = useState(24)
+  const [seconds, setSeconds] = useState(59)
+  const [sessions, setSessions] = useState(1)
   const [displayMessage, setDisplayMessage] = useState(false)
+  const [pause, setPause] = useState(false)
   // const [playOn] = useSound(sound, { volume: 0.5 })
 
   const timerMinutes = minutes < 10 ? `0${minutes}` : minutes.toString()
   const timerSeconds = seconds < 10 ? `0${seconds}` : seconds.toString()
 
-  useEffect(() => {
-    const interval = setInterval(() => {
-      clearInterval(interval)
-      if (seconds === 0) {
-        if (minutes !== 0) {
-          setSeconds(59)
-          setMinutes(minutes - 1)
-        } else {
-          const minutes = displayMessage ? 24 : 4
-          const seconds = 59
+  function reducerProp (number: number) {
+    if (number > 0) {
+      return number - 1
+    } else {
+      return 0
+    }
+  }
 
-          setSeconds(seconds)
-          setMinutes(minutes)
-          setDisplayMessage(!displayMessage)
+  function addProp (number: number) {
+    if (number > 60) {
+      return 1
+    } else {
+      return (number += 1)
+    }
+  }
+
+  useEffect(() => {
+    if (pause && sessions > 0) {
+      const interval = setInterval(() => {
+        clearInterval(interval)
+        if (seconds === 0) {
+          if (minutes !== 0) {
+            setSeconds(59)
+            setMinutes(minutes - 1)
+          } else {
+            const minutes = displayMessage ? 24 : 4
+            const seconds = 59
+
+            setSeconds(seconds)
+            setMinutes(minutes)
+            setSessions(sessions - 1)
+            setDisplayMessage(!displayMessage)
+          }
+        } else {
+          setSeconds(seconds - 1)
         }
-      } else {
-        setSeconds(seconds - 1)
-      }
-    }, 1000)
-  }, [seconds])
+      }, 1000)
+    }
+  }, [seconds, pause])
 
   return (
     <div className="Timer">
-      <h2 className="Title">Pomodoro Timer</h2>
-      <h2 className="Clock">{timerMinutes}:{timerSeconds}</h2>
+      <span className="Title">Pomodoro</span>
 
-    <span className="c-circular-progress c-circular-progress--4"></span>
+      <div className='box-timer'>
+        <div className="minutes">
+          <span>Minutes</span>
+          <div className="set-minutes">
+            <button
+              disabled={pause}
+              className="add-minutes"
+              onClick={() => setMinutes(addProp(minutes))}
+            >
+              <ArrowCircleUpIcon fontSize='large' />
+            </button>
 
+            <div
+              className='box-minutes'
+            >
+              {timerMinutes}
+            </div>
+
+            <button
+              disabled={pause}
+              className="rm-minutes"
+              onClick={() => setMinutes(reducerProp(minutes))}
+            >
+              <ArrowCircleDownIcon fontSize='large' />
+            </button>
+          </div>
+        </div>
+
+        <div className="seconds">
+          <span>Seconds</span>
+          <div
+            className="set-seconds"
+          >
+            <button
+              disabled={pause}
+              className="add-seconds"
+              onClick={() => setSeconds(addProp(seconds))}
+            >
+              <ArrowCircleUpIcon fontSize='large' />
+            </button>
+
+            <div
+              className='box-seconds'
+            >
+              {timerSeconds}
+            </div>
+
+            <button
+              disabled={pause}
+              className="rm-seconds"
+              onClick={() => setSeconds(reducerProp(seconds))}
+            >
+              <ArrowCircleDownIcon fontSize='large' />
+            </button>
+          </div>
+        </div>
+
+        <div className="sessions">
+          <span>Sessions</span>
+          <div className="set-sessions">
+            <button
+              disabled={pause}
+              className="add-sessions"
+              onClick={() => setSessions(addProp(sessions))}
+            >
+              <ArrowCircleUpIcon fontSize='large' />
+            </button>
+
+            <div
+              className='box-sessons'
+            >
+              {sessions}
+            </div>
+
+            <button
+              disabled={pause}
+              className="rm-sessions"
+              onClick={() => setSessions(reducerProp(sessions))}
+            >
+              <ArrowCircleDownIcon fontSize='large' />
+            </button>
+          </div>
+        </div>
+      </div>
+      <button
+        onClick={() => {
+          if (sessions > 0) {
+            setPause(!pause)
+          } else {
+            setPause(false)
+          }
+        }}
+        className="Clock"
+      >
+        {pause === true ? 'Pausar' : 'Continuar'}
+      </button>
     </div>
   )
 }
